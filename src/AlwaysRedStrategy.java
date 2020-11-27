92dd8b4fc59e1b5f778b3209abb1c3d410757580
NOBIPROG <ispannorbert@gmail.com>
2020-11-27 13:59:56 +0100

    Rulett wining prices

--- src/AlwaysRedStrategy.java
+++ src/AlwaysRedStrategy.java
@@ -3,8 +3,9 @@ public class AlwaysRedStrategy implements Strategy {
 
     @Override
     public void bet(Rulett rulett, Person person) {
-        if (Rulett.getRound() == 1) {
+        if (rulett.getRound() == 1) {
             person.setBet(100);
+            rulett.red(person.getBet());
         } else {
             rulett.red(person.getBet());
             person.setBet(person.getBet() * 2);
